da1ebae106bd162b1757f46c936838d157e77904
lxdeng <buyibabe@gmail.com>
2016-08-24 16:42:56 -0700

    http server with template

---
+++ http/clientserver/server_template/server_template.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
+)
+
+// templ represents a single template
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTP handles the HTTP request.
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Println("enter templateHandler.ServeHTTP")
+	// ensure init only once
+	t.once.Do(func() {
+		log.Println("  template.Must in sync.Once")
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	fmt.Println("ListenAndServe: 8080")
+
+	// start the web server
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
