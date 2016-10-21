e2c7ddf5d84a17eacd76355f5179d81919742cfb
lxdeng <buyibabe@gmail.com>
2016-10-21 11:17:36 -0700

    More slice

--- language/slice/slice.go
+++ language/slice/slice.go
@@ -11,10 +11,24 @@ func main() {
 	// or allocate a new underlying array, if the cap is not sufficient
 	testAppend2()
 
+	testAppendAnotherSlice()
+
+	testAppendStringToByteSlice()
+
 	testNilSlice()
 }
 
+func testAppendAnotherSlice() {
+	fmt.Println("testAppendAnotherSlice")
+	s1 := []int{1, 2, 3}
+	s2 := []int{4, 5, 6}
+	s3 := append(s1, s2...)
+	fmt.Println(s3)
+	fmt.Println()
+}
+
 func testAppend() {
+	fmt.Println("testAppend")
 	s := make([]string, 0, 5)
 	fmt.Printf("cap(s)=%d, len(s)=%d\n", cap(s), len(s))
 	s1 := append(s, "a")
@@ -24,6 +38,7 @@ func testAppend() {
 }
 
 func testAppend2() {
+	fmt.Println("testAppend2")
 	a := make([]int, 8, 8)
 	for i := 0; i < 8; i++ {
 		a[i] = i
@@ -41,9 +56,19 @@ func Test(slice []int) {
 	fmt.Printf("cap(the new slice)=%d\n", cap(slice))
 }
 
+// As a special case, it is legal to append a string to a byte slice
+func testAppendStringToByteSlice() {
+	fmt.Println("testAppendStringToByteSlice")
+	s := []byte("hello")
+	s = append(s, " world"...)
+	fmt.Println(s)
+	fmt.Println()
+}
+
 func testNilSlice() {
-	//nilSlice := []int(nil)
-	var nilSlice []int
+	fmt.Println("testNilSlice")
+	nilSlice := []int(nil)
+	//var nilSlice []int
 	fmt.Printf("cap(nilSlice)=%d,  len(nilSlice)=%d\n", cap(nilSlice), len(nilSlice))
 
 	//emptySlice := []int{}
