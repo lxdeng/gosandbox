570275384beca09abcc6d57ab3a10cba658ea1b9
lxdeng <buyibabe@gmail.com>
2017-11-08 15:38:28 -0800

    anonymous function

--- language/function/func.go
+++ language/function/func.go
@@ -9,8 +9,24 @@ func main() {
 	//  and they may be assigned to variables or passed to or returned from functions.
 	f := square
 	fmt.Printf("f's type is %T\n", f)
+
+	test(square)
+	test(f)
+
+	// anonymous function
+	test(func(x int) int {
+		return x * x
+	})
 }
 
 func square(x int) int {
 	return x * x
+}
+
+func square2(x int) (int, error) {
+	return x * x, nil
+}
+
+func test(f func(int) int) {
+	fmt.Printf("%s\n", f)
 }
