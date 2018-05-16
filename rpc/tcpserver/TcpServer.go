2a8060e701d1c84283525f8f8fcb35a4de837d35
lxdeng <buyibabe@gmail.com>
2018-05-16 14:51:35 -0700

    rpc: tcp server and client

---
+++ rpc/tcpserver/TcpServer.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"github.com/lxdeng/gosandbox/rpc/shared"
+	"log"
+	"net"
+	"net/rpc"
+)
+
+func registerArith(server *rpc.Server, arith shared.Arith) {
+	// registers Arith interface by name of `Arithmetic`.
+	// If you want this name to be same as the type name, you
+	// can use server.Register instead.
+	server.RegisterName("Arithmetic", arith)
+}
+
+func main() {
+
+	//Creating an instance of struct which implement Arith interface
+	arith := new(shared.ArithImpl)
+
+	// Register a new rpc server (In most cases, you will use default server only)
+	// And register struct we created above by name "ArithImpl"
+	// The wrapper method here ensures that only structs which implement Arith interface
+	// are allowed to register themselves.
+	server := rpc.NewServer()
+	registerArith(server, arith)
+
+	// Listen for incoming tcp packets on specified port.
+	l, e := net.Listen("tcp", ":1234")
+	if e != nil {
+		log.Fatal("listen error:", e)
+	}
+
+	// This statement links rpc server to the socket, and allows rpc server to accept
+	// rpc request coming from that socket.
+	server.Accept(l)
+}
